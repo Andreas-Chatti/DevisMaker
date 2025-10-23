e492c1cde26ee00871ee8b100a5e58948c2b11f3
Anzzas <blackmindeu@gmail.com>
2025-10-23 18:29:03 +0200

    #minor change

--- DevisMaker/src/ui/companyInfoDialog.h
+++ DevisMaker/src/ui/companyInfoDialog.h
@@ -12,16 +12,16 @@ public:
     CompanyInfoDialog(User& user, QWidget* parent = nullptr);
     ~CompanyInfoDialog() = default;
 
-    const QString& getCompanyName() const { return ui.companyNameLineEdit->text(); }
-    const QString& getCompanyAddress() const { return ui.companyAddressTextEdit->toPlainText(); }
-    const QString& getCompanyPhone() const { return ui.companyPhoneLineEdit->text(); }
-    const QString& getCompanyEmail() const { return ui.companyEmailLineEdit->text(); }
-    const QString& getCompanySiret() const { return ui.companySiretLineEdit->text(); }
-    const QString& getManagerName() const { return ui.managerNameLineEdit->text(); }
-    const QString& getManagerPhone() const { return ui.managerPhoneLineEdit->text(); }
-    const QString& getManagerEmail() const { return ui.managerEmailLineEdit->text(); }
-    const QString& getCompanyApe() const { return ui.companyApeLineEdit->text(); }
-    const QString& getCompanyTva() const { return ui.companyTvaLineEdit->text(); }
+    QString getCompanyName() const { return ui.companyNameLineEdit->text(); }
+    QString getCompanyAddress() const { return ui.companyAddressTextEdit->toPlainText(); }
+    QString getCompanyPhone() const { return ui.companyPhoneLineEdit->text(); }
+    QString getCompanyEmail() const { return ui.companyEmailLineEdit->text(); }
+    QString getCompanySiret() const { return ui.companySiretLineEdit->text(); }
+    QString getManagerName() const { return ui.managerNameLineEdit->text(); }
+    QString getManagerPhone() const { return ui.managerPhoneLineEdit->text(); }
+    QString getManagerEmail() const { return ui.managerEmailLineEdit->text(); }
+    QString getCompanyApe() const { return ui.companyApeLineEdit->text(); }
+    QString getCompanyTva() const { return ui.companyTvaLineEdit->text(); }
 
 private slots:
 
