ae8054599ba6849614c74210bb398b0bfdc085d2
Anzzas <blackmindeu@gmail.com>
2025-11-11 20:22:45 +0100

    Add custom message handler for logging to file
    - Implement customMessageHandler to redirect Qt logs to app.log
    - Log all message types (Debug, Info, Warning, Critical) with timestamps
    - Use QCoreApplication::applicationDirPath() for portable log file location
    - Install message handler at application startup

--- DevisMaker/src/main.cpp
+++ DevisMaker/src/main.cpp
@@ -2,9 +2,32 @@
 #include "utils/FileManager.h"
 #include <QApplication>
 
+void customMessageHandler(QtMsgType type, const QMessageLogContext&, const QString& msg)
+{
+    QString level;
+    switch (type) 
+    {
+    case QtDebugMsg:    level = "DEBUG"; break;
+    case QtInfoMsg:     level = "INFO"; break;
+    case QtWarningMsg:  level = "WARN"; break;
+    case QtCriticalMsg: level = "ERROR"; break;
+    default: return;
+    }
+
+    QString timestamp{ QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss") };
+    QString logMessage{ QString("%1 [%2] %3\n").arg(timestamp, level, msg) };
+
+    QString logPath{ QCoreApplication::applicationDirPath() + "/app.log" };
+    QFile logFile(logPath);
+    logFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    logFile.write(logMessage.toUtf8());
+}
+
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+
+    qInstallMessageHandler(customMessageHandler);
 
     FileManager::ensureDirectoryStructure();
 
